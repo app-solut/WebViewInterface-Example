c4ce5e26d265d97744d554a30ac2ad3cfd182ebb
thorben <thorben@dhcp-10-238-215-164.w1>
2011-03-24 11:39:54 +0100

    Initial Commit

---
+++ App-Solut WebViewInterface/App_Solut_WebViewInterfaceAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  App_Solut_WebViewInterfaceAppDelegate.h
+//  App-Solut WebViewInterface
+//
+//  Created by thorben on 24.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class App_Solut_WebViewInterfaceViewController;
+
+@interface App_Solut_WebViewInterfaceAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet App_Solut_WebViewInterfaceViewController *viewController;
+
+@end
---
+++ App-Solut WebViewInterface/App_Solut_WebViewInterfaceViewController.h
@@ -0,0 +1,15 @@
+//
+//  App_Solut_WebViewInterfaceViewController.h
+//  App-Solut WebViewInterface
+//
+//  Created by thorben on 24.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface App_Solut_WebViewInterfaceViewController : UIViewController {
+    
+}
+
+@end
